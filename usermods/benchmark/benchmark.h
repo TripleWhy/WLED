6042cef3e309242437073d8a2bdc8de4524a33eb
TripleWhy <TripleWhy@users.noreply.github.com>
2023-12-31 15:38:55 +0100

    Add some regular math benchmarks

--- usermods/benchmark/benchmark.h
+++ usermods/benchmark/benchmark.h
@@ -4,8 +4,7 @@
 
 #define NAMED_FUNCTION(F) F, #F
 
-class Benchmark : public Usermod
-{
+class Benchmark : public Usermod {
   private:
     bool enabled = false;
     bool initDone = false;
@@ -16,47 +15,67 @@ class Benchmark : public Usermod
     static const char _name[];
 
   public:
-    void setup()
-    {
+    void setup() {
       initDone = true;
     }
 
-    void addToConfig(JsonObject& root)
-    {
+    void addToConfig(JsonObject& root) {
       JsonObject top = root.createNestedObject(FPSTR(_name));
       top["enabled"] = enabled;
     }
 
-    bool readFromConfig(JsonObject& root)
-    {
+    bool readFromConfig(JsonObject& root) {
       JsonObject top = root[FPSTR(_name)];
       bool configComplete = !top.isNull();
       configComplete &= getJsonValue(top["enabled"], enabled);
       return configComplete;
     }
 
-    uint16_t getId()
-    {
+    uint16_t getId() {
       return 43;
     }
 
-    void loop()
-    {
+    void loop() {
       if (!enabled || strip.isUpdating()) return;
 
       const unsigned long now = millis();
-      if ((index == 0) && (now - previousTimeStamp < 10000)) return;
+      if ((index == 0) && (now - previousTimeStamp < 20000)) return;
       previousTimeStamp = now;
 
-      switch (index)
-      {
-        case 0: measure(NAMED_FUNCTION(&sin8)); break;
-        case 1: measure(NAMED_FUNCTION(&sin16)); break;
-        case 2: measure(&Benchmark::benchFloatTrig, NAMED_FUNCTION(&sin66868438)); break;
-        case 3: measure(&Benchmark::benchFloatTrig, NAMED_FUNCTION(&cos28050328<float>)); break;
-        case 4: measure(&Benchmark::benchFloatTrig, NAMED_FUNCTION(&std::sin)); break;
-        case 5: measure(&Benchmark::benchFloatTrig, NAMED_FUNCTION(&sinf)); break;
-        case 6: measure(&Benchmark::benchFloatTrig, NAMED_FUNCTION(&sin_t)); break;
+      switch (index) {
+        case  0: measure(NAMED_FUNCTION(&Benchmark::benchEmpty)); break;
+        case  1: measure(NAMED_FUNCTION(&Benchmark::benchEmptyLoop)); break;
+        case  2: measure(&Benchmark::benchFloat, NAMED_FUNCTION(&Benchmark::identityF)); break;
+        case  3: measure(NAMED_FUNCTION(&Benchmark::identityUint16)); break;
+        case  4: measure(NAMED_FUNCTION(&::sin8)); break;
+        case  5: measure(NAMED_FUNCTION(&::sin16)); break;
+        case  6: measure(&Benchmark::benchFloatTrig, NAMED_FUNCTION(&Benchmark::sin66868438)); break;
+        case  7: measure(&Benchmark::benchFloatTrig, NAMED_FUNCTION(&Benchmark::cos28050328<float>)); break;
+        case  8: measure(&Benchmark::benchFloatTrig, NAMED_FUNCTION(&std::sin)); break;
+        case  9: measure(&Benchmark::benchFloatTrig, NAMED_FUNCTION(&::sinf)); break;
+        case 10: measure(&Benchmark::benchFloatTrig, NAMED_FUNCTION(&::sin_t)); break;
+        case 11: measure(&Benchmark::benchFloat, NAMED_FUNCTION(&Benchmark::addF)); break;
+        case 12: measure(&Benchmark::benchFloat, NAMED_FUNCTION(&Benchmark::subtractF)); break;
+        case 13: measure(&Benchmark::benchFloat, NAMED_FUNCTION(&Benchmark::multiplyF)); break;
+        case 14: measure(&Benchmark::benchFloat, NAMED_FUNCTION(&Benchmark::divideF)); break;
+        case 15: measure(&Benchmark::benchFloat, NAMED_FUNCTION(&Benchmark::fmaManual)); break;
+        case 16: measure(&Benchmark::benchFloat, NAMED_FUNCTION(&Benchmark::fmaLib)); break;
+        case 17: measure(NAMED_FUNCTION(&Benchmark::addUint8)); break;
+        case 18: measure(NAMED_FUNCTION(&Benchmark::subtractUint8)); break;
+        case 19: measure(NAMED_FUNCTION(&Benchmark::multiplyUint8)); break;
+        case 20: measure(NAMED_FUNCTION(&Benchmark::divideUint8)); break;
+        case 21: measure(NAMED_FUNCTION(&Benchmark::addUint16)); break;
+        case 22: measure(NAMED_FUNCTION(&Benchmark::subtractUint16)); break;
+        case 23: measure(NAMED_FUNCTION(&Benchmark::multiplyUint16)); break;
+        case 24: measure(NAMED_FUNCTION(&Benchmark::divideUint16)); break;
+        case 25: measure(NAMED_FUNCTION(&Benchmark::addUint32)); break;
+        case 26: measure(NAMED_FUNCTION(&Benchmark::subtractUint32)); break;
+        case 27: measure(NAMED_FUNCTION(&Benchmark::multiplyUint32)); break;
+        case 28: measure(NAMED_FUNCTION(&Benchmark::divideUint32)); break;
+        case 29: measure(NAMED_FUNCTION(&Benchmark::addInt)); break;
+        case 30: measure(NAMED_FUNCTION(&Benchmark::subtractInt)); break;
+        case 31: measure(NAMED_FUNCTION(&Benchmark::multiplyInt)); break;
+        case 32: measure(NAMED_FUNCTION(&Benchmark::divideInt)); break;
         default:
           index = 0;
           return;
@@ -64,49 +83,60 @@ class Benchmark : public Usermod
       ++index;
     }
 
-    template<typename OutType, typename InType>
-    static void measure(int (*benchmarkFunction)(OutType (*)(InType)), OutType (*function)(InType), const char * name)
-    {
-        const unsigned long t0 = millis();
-        const int count = (*benchmarkFunction)(function);
-        const unsigned long t1 = millis();
+    static void printMeasurementResults(const unsigned long t0, const unsigned long t1, int count, const char * name) {
         Serial.print(name);
         Serial.print(": ");
-#ifdef ARDUINO_ARCH_ESP32
-        Serial.print(((t1 - t0) * 1000) / (float)count * 1000.0f, std::numeric_limits<float>::digits10);
+// #ifdef ARDUINO_ARCH_ESP32
+        // Serial.print(((t1 - t0) * 1000) / (float)count * 1000.0f, std::numeric_limits<float>::digits10);
+        Serial.print(((t1 - t0) * 1000) / (float)count * 1000.0f);
         Serial.print(" ns (");
-#else
-        Serial.print(((t1 - t0) * 1000) / (float)count, std::numeric_limits<float>::digits10);
-        Serial.print(" us (");
-#endif
+// #else
+//         Serial.print(((t1 - t0) * 1000) / (float)count, std::numeric_limits<float>::digits10);
+//         Serial.print(" us (");
+// #endif
         Serial.print(t1 - t0);
         Serial.print(" ms / ");
         Serial.print(count);
         Serial.println(" iterations)");
     }
 
-    static void measure(uint8_t (*function)(uint8_t), const char * name)
-    {
+    template<typename OutType, typename InType>
+    static void measure(int (*benchmarkFunction)(OutType (*)(InType)), OutType (*function)(InType), const char * name) {
+        const unsigned long t0 = millis();
+        const int count = (*benchmarkFunction)(function);
+        const unsigned long t1 = millis();
+        printMeasurementResults(t0, t1, count, name);
+    }
+
+    static void measure(int (*function)(), const char * name) {
+        const unsigned long t0 = millis();
+        const int count = (*function)();
+        const unsigned long t1 = millis();
+        printMeasurementResults(t0, t1, count, name);
+    }
+
+    static void measure(uint8_t (*function)(uint8_t), const char * name) {
         measure(&Benchmark::benchUint8, function, name);
     }
 
-    static void measure(uint16_t (*function)(uint16_t), const char * name)
-    {
+    static void measure(uint16_t (*function)(uint16_t), const char * name) {
         measure(&Benchmark::benchUint16, function, name);
     }
 
-    static void measure(int16_t (*function)(uint16_t), const char * name)
-    {
+    static void measure(int16_t (*function)(uint16_t), const char * name) {
         measure(&Benchmark::benchUint16, function, name);
+    }
+
+    static void measure(uint32_t (*function)(uint32_t), const char * name) {
+        measure(&Benchmark::benchUint32, function, name);
+    }
+
+    static void measure(int (*function)(int), const char * name) {
+        measure(&Benchmark::benchInt, function, name);
     }
 
     static int benchFloat(float (*fn)(float)) {
       static_assert(sizeof(float) == sizeof(uint32_t), "sizes don't match");
-      union U
-      {
-        uint32_t i;
-        float f;
-      };
       constexpr uint32_t posMin = 0b00000000100000000000000000000000;
       constexpr uint32_t posMax = 0b01111111011111111111111111111111;
       int counter = 0;
@@ -143,6 +173,20 @@ class Benchmark : public Usermod
       return (uMax.i - uMin.i + 1) / increment + 1;
     }
 
+    static int benchEmpty() {
+      return 1;
+    }
+
+    static int benchEmptyLoop() {
+      constexpr int repetitionCount = 35;
+      for (int repetitions = 0; repetitions < repetitionCount; ++repetitions) {
+        uint16_t i = 0;
+        do {
+        } while (++i != 0);
+      }
+      return (((int)std::numeric_limits<uint16_t>::max()) + 1) * repetitionCount;
+    }
+
     static int benchUint8(uint8_t (*fn)(uint8_t)) {
       constexpr int repetitionCount = 35 * 256;
       for (int repetitions = 0; repetitions < repetitionCount; ++repetitions) {
@@ -176,9 +220,39 @@ class Benchmark : public Usermod
       return (((int)std::numeric_limits<uint16_t>::max()) + 1) * repetitionCount;
     }
 
+    static int benchUint32(uint32_t (*fn)(uint32_t)) {
+      constexpr uint32_t increment = (uint32_t)(std::numeric_limits<uint16_t>::max()) / uint32_t(35);
+      constexpr uint32_t limit = std::numeric_limits<uint32_t>::max() - increment;
+      for (uint32_t i = 0; ; i += increment) {
+        (*fn)(i);
+        if (limit < i) {
+          break;
+        }
+      }
+      return std::numeric_limits<uint32_t>::max() / increment + 1;
+    }
+
+    static int benchInt(int (*fn)(int)) {
+      constexpr int increment = (int)(std::numeric_limits<uint16_t>::max()) / 50;
+      constexpr int limit = std::numeric_limits<int>::max() - increment;
+      for (int i = 0; ; i += increment) {
+        (*fn)(i);
+        if (limit < i) {
+          break;
+        }
+      }
+      return std::numeric_limits<int>::max() / increment + 1;
+    }
+
+    static uint16_t identityUint16(uint16_t x) {
+      return x;
+    }
+    static float identityF(float x) {
+      return x;
+    }
+
     // https://stackoverflow.com/a/66868438
-    static float sin66868438(float x)
-    {
+    static float sin66868438(float x) {
         const float B = 4.0f/float(M_PI);
         const float C = -4.0f/float(M_PI*M_PI);
 
@@ -195,8 +269,7 @@ class Benchmark : public Usermod
 
     // https://stackoverflow.com/a/28050328
     template<typename T>
-    static inline T cos28050328(T x) noexcept
-    {
+    static inline T cos28050328(T x) noexcept {
         constexpr T tp = 1./(2.*M_PI);
         x *= tp;
         x -= T(.25) + std::floor(x + T(.25));
@@ -205,6 +278,77 @@ class Benchmark : public Usermod
         x += T(.225) * x * (std::abs(x) - T(1.));
         #endif
         return x;
+    }
+
+    static float addF(float f) {
+      return f + 2.0f;
+    }
+    static float subtractF(float f) {
+      return f - 2.0f;
+    }
+    static float multiplyF(float f) {
+      return f * 1.001f;
+    }
+    static float divideF(float f) {
+      return 1.0f / f;
+    }
+    static float fmaManual(float f) {
+      return f * f + 42.0f;
+    }
+    static float fmaLib(float f) {
+      return fmaf(f, f, 42.0f);
+    }
+
+    static uint8_t addUint8(uint8_t i) {
+      return i + uint8_t(42u);
+    }
+    static uint8_t subtractUint8(uint8_t i) {
+      return i - uint8_t(42u);
+    }
+    static uint8_t multiplyUint8(uint8_t i) {
+      return i * uint8_t(42u);
+    }
+    static uint8_t divideUint8(uint8_t i) {
+      return i / uint8_t(42u);
+    }
+
+    static uint16_t addUint16(uint16_t i) {
+      return i + uint16_t(2u);
+    }
+    static uint16_t subtractUint16(uint16_t i) {
+      return i - uint16_t(2u);
+    }
+    static uint16_t multiplyUint16(uint16_t i) {
+      return i * uint16_t(2u);
+    }
+    static uint16_t divideUint16(uint16_t i) {
+      return i / uint16_t(2u);
+    }
+
+    static uint32_t addUint32(uint32_t i) {
+      return i + uint32_t(2u);
+    }
+    static uint32_t subtractUint32(uint32_t i) {
+      return i - uint32_t(2u);
+    }
+    static uint32_t multiplyUint32(uint32_t i) {
+      return i * uint32_t(2u);
+    }
+    static uint32_t divideUint32(uint32_t i) {
+      return i / uint32_t(2u);
+    }
+
+    static int addInt(int i) {
+      return i + int(2u);
+    }
+    static int subtractInt(int i) {
+      return i - int(2u);
+    }
+    static int multiplyInt(int i) {
+      return i * int(2u);
+    }
+    static int divideInt(int i) {
+      return i / int(2u);
     }
 };
 
